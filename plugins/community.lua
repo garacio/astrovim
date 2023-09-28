a03edb40fd1ae25ba61307b0d4bc2c8525d8dbdf
Denis Krasnoper <garacio@gmail.com>
2023-09-28 23:27:12 +0200

    + astrocommunity

---
+++ plugins/community.lua
@@ -0,0 +1,15 @@
+return {
+  -- Add the community repository of plugin specifications
+  "AstroNvim/astrocommunity",
+  -- example of importing a plugin, comment out to use it or add your own
+  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.helm" },
+}
