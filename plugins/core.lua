bcc35306db05d87f12c08538dc7530b467c59992
Denis Krasnoper <garacio@gmail.com>
2023-09-28 21:52:14 +0200

    Fix identation

--- plugins/core.lua
+++ plugins/core.lua
@@ -3,16 +3,16 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts) 
       opts.filesystem.filtered_items = {
-  	    visible = true,
+        visible = true,
         show_hidden_count = true,
-  	    hide_dotfiles = false,
-        hide_gitignored = true,
-	      hide_by_name = {
-	        -- '.git',
-	        -- '.DS_Store',
-  	      -- 'thumbs.db',
-	      },
-	      never_show = {},
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_by_name = {
+          -- '.git',
+          -- '.DS_Store',
+          -- 'thumbs.db',
+        },
+        never_show = {},
       }
       return opts
     end,
